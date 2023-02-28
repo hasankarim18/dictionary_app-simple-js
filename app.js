0e6cdd391ceef1aa27f7d7638093b1ef4ac9d350
hasankarim18 <hallysust@gmail.com>
2023-02-28 23:59:46 +0600

    dictionary app complete

--- app.js
+++ app.js
@@ -1,19 +1,26 @@
-
+console.log('loading');
 
 // load and display data
-const loadData = async (searchWrod )=> {
+const loadData =  (searchWrod )=> {
 
-    try {
-        const res = await fetch(
-          "https://api.dictionaryapi.dev/api/v2/entries/en/" + searchWrod
-        );
-        const data = await res.json();
+  if(searchWrod != ''){
+       fetch("https://api.dictionaryapi.dev/api/v2/entries/en/" + searchWrod)
+         .then((res) => res.json())
+         .then((data) => {
+           // console.log(data);
+           if (data.title === "No Definitions Found") {
+             displayError(data.title);
+           } else {
+             displayData(data);
+           }
+         })
+         .catch((err) => {
+           console.log(err);
+         });
+  }else {
+    alert('Input cant be left empty!!!');
+  }
 
-        displayData(data)
-
-    } catch (error) {
-      console.log(error);
-    }
 }
 
 const searchBtn = document.getElementById("search_btn");
@@ -25,6 +32,8 @@ searchBtn.addEventListener('click', function () {
     const inputValue = inputField.value 
 
     loadData(inputValue)
+
+   
 
 })
 
@@ -45,29 +54,102 @@ document
 //     document.getElementById("player").play();
 // })
 
-
-
-const displayData = (data)=> {
-  console.log(data);  
-
-  const displayContainer = document.getElementById("display_container");
-    displayContainer.innerHTML = "";
-
-    const dataEl = document.createElement('div');
-    dataEl.classList.add("data", "bg-white", "rounded");
-
-
-    const innerContent = `
-       <div class="d-flex align-items-center" >
-                <h1></h1> <audio src=""></audio> <button class="btn"><i class="fa-solid fa-volume-high"></i></button>
-      </div>
-    `;
-
-    dataEl.innerHTML = innerContent;
-
-    displayContainer.appendChild(dataEl);
-
+const displayError = (error)=> {
+  alert(error);
 }
 
+const displayData = (response)=> {
+  const data = response[0];
+ // console.log(data);
+  const { word, phonetics, meanings, phonetic } = data;
+  const  audio  = phonetics[0]?.audio;
+
+  //  console.log(meanings[0]);
+  const { partOfSpeech, antonyms, synonyms, definitions } = meanings[0];
+  // console.log(definitions);
+
+
+
+  const displayContainer = document.getElementById("display_container");
+  displayContainer.innerHTML = "";
+
+  const dataEl = document.createElement("div");
+  dataEl.classList.add("data", "bg-white", "rounded");
+
+  const innerContent = `
+       <div  class="data bg-white rounded p-3">
+                    <!-- inner content start -->
+                    <div class="d-flex align-items-center" >
+                        <h1>${word}</h1>
+                        <div class="ms-2">${phonetic? phonetic: null}</div>
+                        <div class="ms-2">
+                            <audio id="player" src="${audio}"></audio> 
+                           <button id="playerBtn" class="btn">
+                            ${
+                                audio ?  `<i class="fa-solid fa-volume-high"></i>`:"Audio unavailable"
+                            }                            
+                            </button>                             
+                        </div>
+                        
+                    </div>
+                    <hr> 
+                    <h4>${partOfSpeech ? partOfSpeech: 'No part of speech found'}</h4>
+                    <ul>
+                        ${
+                          definitions?
+                          definitions.map(
+                          (item) => `
+                            <li class="border-bottom p-2">
+                                <p class="m-0 p-0">
+                                   <strong> Definition:</strong> ${
+                                     item.definition 
+                                   }
+                                </p>
+                                <p class="m-0 p-0">
+                                   <strong> Exmaple:</strong> ${
+                                     item.example
+                                       ? item.example
+                                       : "No example available"
+                                   }
+                                </p>
+                            </li>
+                          `
+                        ):
+                        `No definition found`
+
+                      }                            
+                    </ul>
+                    <div>
+                        <p>Synnonyms:</p>
+                        <p>Antonymss:</p>
+                    </div>
+                </div>
+    `;
+  dataEl.innerHTML = innerContent;
+
+  displayContainer.appendChild(dataEl);
+  // audioCall()\
+
+   document.getElementById("playerBtn").addEventListener("click", () => {
+     document.getElementById("player").play();
+   });
+}
+
+// const audioCall = ()=> {
+
+//   document.getElementById("playerBtn").addEventListener('click', ()=> {
+//     document.getElementById("player").play()
+//   })
+
+// }
+
+//  document.getElementById("playerBtn").addEventListener("click", () => {
+//    document.getElementById("player").play();
+//  });
+
+
+ // loadData('town')
+
+ console.log('loaded');
 
 
